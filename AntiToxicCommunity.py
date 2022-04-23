ec86cd1c28dc286f569b6bbf0ecd2018a53f6efe
unknown <danino123yt@gmail.com>
2022-04-23 12:34:42 +0300

    Update version 2

--- AntiToxicCommunity.py
+++ AntiToxicCommunity.py
@@ -1,15 +1,25 @@
 import pyautogui
+import keyboard
 from pynput.keyboard import Key, Listener
+from time import sleep
 
 TriggerEventKeyBind = Key.delete
+AllCommand = "/all"
+MessageBeforeMute = "I'm using AntiToxicCommunity and it helps me to stay away from toxic people like you :)"
 muteAllCommand = '/mute all'
 
 def show(key):
 
     if key == TriggerEventKeyBind:
+         keyboard.press_and_release('enter')
+         sleep(0.01)
+         pyautogui.typewrite(AllCommand + " " + MessageBeforeMute)
+         keyboard.press_and_release('enter')
+         sleep(0.01)
+         keyboard.press_and_release('enter')
+         sleep(0.01)
          pyautogui.typewrite(muteAllCommand)
-         pyautogui.press('enter')
+         keyboard.press_and_release('enter')
 
-# Collect all event until released
 with Listener(on_press = show) as listener:   
     listener.join()
